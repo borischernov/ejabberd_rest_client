47bd0ef80028e013297d0d7998885cf6dd2ab9f5
borischernov <boris@imode.lv>
2018-06-13 22:53:31 +0300

    first commit

---
+++ ejabberd_rest_client.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = %q{ejabberd_rest_client}
+  s.version = "0.0.1"
+  s.date = %q{2018-06-13}
+  s.summary = %q{REST client for ejabberd}
+  s.author = "Boris Chernov"
+  s.files = [
+    "lib/ejabberd_rest_client.rb"
+  ]
+  s.require_paths = ["lib"]
+  s.add_dependency("json", '~> 0')
+  s.extra_rdoc_files = ["README.md"]
+end
---
+++ lib/ejabberd_rest_client.rb
@@ -0,0 +1,28 @@
+require 'uri'
+require 'net/http'
+require 'json'
+class EjabberdRestClient
+	def initialize(url, jid, password)
+		@url = url
+		@jid = jid
+		@password = password
+	end
+
+	def request(method, data = {})
+		url = URI.parse(@url)
+		url.path = "/api/#{method}"
+		begin
+			http = Net::HTTP.new(url.host, url.port)
+			request = Net::HTTP::Post.new(url.path)
+			request.basic_auth @jid, @password
+			request.body = data.to_json
+			request["Content-Type"] = 'application/json'
+			request["Accept"] = 'application/json'
+			response = http.request(request)
+			return nil unless response.code.to_i == 200
+			JSON.parse("[#{response.body}]").first
+		rescue
+			$logger.error("Exception while accessing ejabberd API: #{$!}")
+		end
+	end
+end
